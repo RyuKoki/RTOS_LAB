5e487ee1e251db23fc8e1d2856bb749efe8ba314
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2022-05-24 11:14:20 +0700

    Create 05_keypad_rp2040_wokwi.cpp

---
+++ 04_learning_MbedOS/05_keypad_rp2040_wokwi.cpp
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const uint8_t ROWS = 4;
+const uint8_t COLS = 4;
+
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+uint8_t rowPins[ROWS] = { 26, 22, 21, 20 }; // Pins connected to R1, R2, R3, R4
+uint8_t colPins[COLS] = { 19, 18, 17, 16 }; // Pins connected to C1, C2, C3, C4
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial1.begin(115200);
+}
+
+void loop() {
+  char key = keypad.getKey();
+  if ( key != NO_KEY ) {
+    Serial1.println(key);
+  }
+  delay(100);
+}
